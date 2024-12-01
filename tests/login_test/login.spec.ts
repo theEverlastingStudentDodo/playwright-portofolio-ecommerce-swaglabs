8e2bffb5de70f9af25c0f98fc41cc4f8c1650c65
theEverlastingStudentDodo <rahmandika.fardo@gmail.com>
2024-12-01 19:26:56 +0700

    Feat: adding more login test

--- tests/login_test/login.spec.ts
+++ tests/login_test/login.spec.ts
@@ -4,13 +4,58 @@
 import { test, expect } from '@playwright/test';
 import { LoginPage } from '../../pages/login.page';
 import { configValues } from '../../env-config/envConfig';
+import { loginMessages } from '../../messages/loginMessages';
+import { MainPage } from '../../pages/main.page';
 
 
 test.describe('Login Functionality', () => {
     test('Login with valid credential', async ({ page }) => {
-        const loginPage = new LoginPage(page);
+    const loginPage = new LoginPage(page);
+    const mainPage = new MainPage(page);
        await loginPage.navigateToLogin();
        await loginPage.login(configValues.validCreds, configValues.validPass)
+       const header = mainPage.headerField
+       await expect(header).toBeVisible();
+})
+
+test('Login with invalid credential', async ({ page }) => {
+    const loginPage = new LoginPage(page);
+    await loginPage.navigateToLogin();
+    await loginPage.login(configValues.invalidCreds, configValues.invalidPass);
+    const error = loginPage.errorBox;
+    await expect(error).toHaveText(loginMessages.invalidCreds);
+})
+
+test('Login with empty username', async ({ page }) => {
+    const loginPage = new LoginPage(page);
+    await loginPage.navigateToLogin();
+    await loginPage.login('', configValues.validPass);
+    const error = loginPage.errorBox;
+    await expect(error).toHaveText(loginMessages.missingUsername);
+})
+
+test('Login with empty password', async ({ page }) => {
+    const loginPage = new LoginPage(page);
+    await loginPage.navigateToLogin();
+    await loginPage.login(configValues.validCreds, '');
+    const error = loginPage.errorBox;
+    await expect(error).toHaveText(loginMessages.missingPassword);
+})
+
+test('Login with empty username and empty password', async ({ page }) => {
+    const loginPage = new LoginPage(page);
+    await loginPage.navigateToLogin();
+    await loginPage.login('', '');
+    const error = loginPage.errorBox;
+    await expect(error).toHaveText(loginMessages.missingUsername);
+})
+
+test('Login with locked creds', async ({ page }) => {
+    const loginPage = new LoginPage(page);
+    await loginPage.navigateToLogin();
+    await loginPage.login('', '');
+    const error = loginPage.errorBox;
+    await expect(error).toHaveText(loginMessages.missingUsername);
 })
 
 })
