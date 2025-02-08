8c6a598cdefd0a73379226e594973610f490c6fa
theEverlastingStudentDodo <rahmandika.fardo@gmail.com>
2025-02-08 14:04:48 +0700

    feat; adding a test case on add to cart page (not checkout)

--- pages/cart.page.ts
+++ pages/cart.page.ts
@@ -4,6 +4,7 @@ export class CartPage {
 
     private readonly page: Page;
     readonly cartList: Locator;
+    readonly cartItem: Locator;
     readonly removeItemButton: Locator;
     readonly productInTheCart: Locator;
 
@@ -12,12 +13,9 @@ export class CartPage {
     this.cartList = page.locator('[data-test="cart-list"]');
     this.removeItemButton = page.locator('.btn.btn_secondary.btn_small.cart_button');
     this.productInTheCart = page.locator('.inventory_item_name');
+    this.cartItem = page.locator('.cart_item');
   }
 
   //Actions
-  async browseProductInTheCart(productName: string) {
-    const productCardInTheCart = this.productInTheCart.filter({hasText: productName});
-    await productCardInTheCart.hover();
-}
-
+  
 }    
